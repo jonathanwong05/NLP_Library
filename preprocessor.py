ca7ea2a1d23e490499dc14d6b18d67d4e608d372
Vichu Selvaraju <vichuselvaraju@gmail.com>
2024-11-23 14:13:38 -0500

    website processort

--- preprocessor.py
+++ preprocessor.py
@@ -2,6 +2,17 @@ import re
 from collections import Counter
 import textstat
 from textblob import TextBlob
+import requests
+from bs4 import BeautifulSoup
+
+
+def load_website(url, div_class):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        text_divs = soup.find_all("div", class_=div_class)
+        text = "\n".join(div.get_text(separator="\n").strip() for div in text_divs)
+    return text
 
 
 def clean_text(text, stop_words=None):
@@ -16,7 +27,7 @@ def clean_text(text, stop_words=None):
     :return: The cleaned text
     """
     # Remove punctuation
-    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r"[^\w\s]", "", text)
 
     # Lowercase
     text = text.lower()
@@ -29,7 +40,8 @@ def clean_text(text, stop_words=None):
         words = [word for word in words if word not in stop_words]
 
     # Join words back into single string
-    return ' '.join(words)
+    return " ".join(words)
+
 
 def tokenize_text(text):
     """
@@ -38,6 +50,7 @@ def tokenize_text(text):
     :return: List of words
     """
     return text.split()
+
 
 def compute_word_count(text):
     """
@@ -48,6 +61,7 @@ def compute_word_count(text):
     words = tokenize_text(text)
     return Counter(words)
 
+
 def compute_readability(text):
     """
     Compute readability scores for the text
@@ -55,9 +69,10 @@ def compute_readability(text):
     :return: Dictionary containing readability scores
     """
     return {
-        'flesch_reading_ease': textstat.flesch_reading_ease(text),
-        'gunning_fog': textstat.gunning_fog(text)
+        "flesch_reading_ease": textstat.flesch_reading_ease(text),
+        "gunning_fog": textstat.gunning_fog(text),
     }
+
 
 def compute_sentiment(text):
     """
@@ -67,9 +82,10 @@ def compute_sentiment(text):
     """
     blob = TextBlob(text)
     return {
-        'polarity': blob.sentiment.polarity,
-        'subjectivity': blob.sentiment.subjectivity
+        "polarity": blob.sentiment.polarity,
+        "subjectivity": blob.sentiment.subjectivity,
     }
+
 
 def extract_unique_words(text):
     """
@@ -79,3 +95,48 @@ def extract_unique_words(text):
     """
     words = tokenize_text(text)
     return set(words)
+
+
+def website_parser(url, div_class):
+    """Gets information on text from a website
+
+    Args:
+        url (string): Website url
+        div_class (string): Div class to find specific text
+
+    Returns:
+        dictionary: dictionary with text information
+    """
+    results = {}
+
+    # Load the text from a website
+    text = load_website(url, div_class)
+
+    # Clean the text
+    text = clean_text(text)
+
+    # Get word counts
+    word_counts = compute_word_count(text)
+    results["wordcount"] = word_counts
+
+    # Get unique words
+    unique_words = extract_unique_words(text)
+    results["unique words"] = unique_words
+
+    # Get sentiment
+    sentiment = compute_sentiment(text)
+    results.update(sentiment)
+
+    # Get readability
+    readability = compute_readability(text)
+    results.update(readability)
+
+    return results
+
+
+results = website_parser(
+    "https://genius.com/Kanye-west-all-falls-down-lyrics",
+    "Lyrics__Container-sc-1ynbvzw-1 kUgSbL",
+)
+
+print(results)
